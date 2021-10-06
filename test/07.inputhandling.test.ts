c6fa9d660144bd8757be1460e154811b7cd65b79
selvakram <saiselvak@gmail.com>
2021-10-06 13:23:36 +0530

    updated input handling

--- test/07.inputhandling.test.ts
+++ test/07.inputhandling.test.ts
@@ -9,6 +9,7 @@ describe("inTech Session Demo on Playwright", () => {
         const page = await context.newPage();
         await page.goto("https://home-sc10.ucnlabext.com/inContact/Login.aspx?");
         await page.type('id=ctl00_BaseContent_msl_txtUsername', 'agent01.sfdcinternalgbu@SC0010.com');//type function
+        await page.type('id=ctl00_BaseContent_msl_txtUsername', 'test Method');//type function
         await page.fill('id=ctl00_BaseContent_msl_txtUsername', 'agent01.sfdcinternalgbu@SC0010.com'); //fill function
         await page.focus('id=ctl00_BaseContent_msl_txtUsername');
         //findElement
